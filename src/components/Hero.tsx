eb41201df8abcd97c8b8fb6976c407c0244504fe
Yudai NAKATA <yudai.nkt@gmail.com>
2021-06-28 13:25:09 +0900

    feat: change title color to match RFA logo

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ export const Hero = ({ title }: { title: string }) => (
     justifyContent="center"
     alignItems="center"
     height="100vh"
-    bgGradient="linear(to-l, #7928CA, #FF0080)"
+    bgGradient="linear(to-r, #f82303, #fcd701)"
     bgClip="text"
   >
     <Heading fontSize="6vw">{title}</Heading>
