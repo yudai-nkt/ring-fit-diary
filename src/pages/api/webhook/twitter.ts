de37f390d02db1d5ea3f5a49f3d87fbaeca4ded4
Yudai NAKATA <yudai.nkt@gmail.com>
2021-07-04 23:27:44 +0900

    feat: respond to Twitter webhook's CRC validation

---
+++ src/pages/api/webhook/twitter.ts
@@ -0,0 +1,35 @@
+import { createHmac } from "crypto";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req;
+  switch (method) {
+    case "GET": {
+      // GET request is a CRC validation.
+      const crcToken = req.query.crc_token;
+      if (!Array.isArray(crcToken)) {
+        const hmac = createHmac(
+          "sha256",
+          process.env.TWITTER_BOT_API_SECRET_KEY
+        )
+          .update(crcToken)
+          .digest("base64");
+        res.status(200).json({ response_token: `sha256=${hmac}` });
+      } else {
+        res.status(400).send("Invalid CRC token.");
+      }
+      break;
+    }
+    case "POST": {
+      // POST request is an account activity.
+      // To be implemented.
+      break;
+    }
+    default: {
+      res.setHeader("Allow", ["GET", "POST"]);
+      res.status(405).json({ message: `Method ${method} not allowed.` });
+    }
+  }
+};
+
+export default handler;
