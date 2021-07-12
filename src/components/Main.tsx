15f092672079d66702b80977f04205151af5dab6
Yudai NAKATA <yudai.nkt@gmail.com>
2021-07-13 02:08:46 +0900

    feat: tweak index page styling

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,7 @@
 import type { StackProps } from "@chakra-ui/react";
-import { Stack } from "@chakra-ui/react";
+import { VStack } from "@chakra-ui/react";
 import type { VFC } from "react";
 
 export const Main: VFC<StackProps> = (props) => (
-  <Stack spacing="1.5rem" maxWidth="80rem" pt="8rem" px="1rem" {...props} />
+  <VStack spacing="1.5rem" maxWidth="80rem" pt="8rem" px="1rem" {...props} />
 );
