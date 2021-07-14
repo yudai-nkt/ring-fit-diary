89b8e5923114f5e112522d80880c8a6cc51bc6a8
Yudai NAKATA <yudai.nkt@gmail.com>
2021-07-14 23:52:02 +0900

    feat: validate if requests are coming from Twitter

--- src/pages/api/webhook/twitter.ts
+++ src/pages/api/webhook/twitter.ts
@@ -1,5 +1,7 @@
-import { createHmac } from "crypto";
+import { createHmac, timingSafeEqual } from "crypto";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { Err, Ok } from "neverthrow";
+import type { Result } from "neverthrow";
 import type { TweetV1 } from "twitter-api-v2";
 
 type AtMentionPayLoad = {
@@ -29,6 +31,28 @@ const isAtMentionPayload = (arg: unknown): arg is AtMentionPayLoad => {
     Object.keys(arg).every((key) => keysOfAtMentionPayload.includes(key))
   );
 };
+
+const validateSignatureHeader: (req: NextApiRequest) => Result<boolean, Error> =
+  ({ headers, body }) => {
+    const signature = headers["x-twitter-webhooks-signature"];
+    if (Array.isArray(signature) || signature === undefined) {
+      return new Err(Error("Incorrect type of signature in header."));
+    } else {
+      const calculatedSignature = `sha256=${createHmac(
+        "sha256",
+        TWITTER_BOT_API_SECRET_KEY
+      )
+        .update(JSON.stringify(body))
+        .digest("base64")}`;
+
+      return new Ok(
+        timingSafeEqual(
+          Buffer.from(signature),
+          Buffer.from(calculatedSignature)
+        )
+      );
+    }
+  };
 
 /**
  * Returns whether or not media attached to a tweet should be processed with Cloud Vision API.
@@ -71,17 +95,35 @@ const handler = (req: NextApiRequest, res: NextApiResponse): void => {
     }
     case "POST": {
       // POST request is an account activity.
-      if (isAtMentionPayload(body)) {
-        const atMention = body.tweet_create_events[0];
-        if (shouldBeSentToCloudVision(atMention)) {
-          res.status(200).json({ message: "Related to Ring Fit Adventure." });
-        } else {
-          res
-            .status(400)
-            .json({ message: "Not related to Ring Fit Adventure." });
-        }
-      } else {
+      const isSentFromTwitter = validateSignatureHeader(req);
+
+      if (isSentFromTwitter.isErr()) {
+        // Signature was either malformed or absent.
+        res.status(400).json({ message: isSentFromTwitter.error.message });
+        break;
+      }
+
+      if (!isSentFromTwitter.value) {
+        // Signature was present, but an invalid one.
+        res.status(400).json({ message: "Invalid signature in header." });
+        break;
+      }
+
+      if (!isAtMentionPayload(body)) {
+        // Webhook was fired due to other activities.
         res.status(400).json({ message: "Not an @mention payload." });
+        break;
+      }
+
+      const atMention = body.tweet_create_events[0];
+      if (shouldBeSentToCloudVision(atMention)) {
+        // Picture(s) of RFA were shared via Nintendo Switch, so they'll be sent to
+        // Cloud Vison API.
+        res.status(200).json({ message: "Related to Ring Fit Adventure." });
+      } else {
+        // @ringfitdiary got mentioned, but the tweet was neither related to RFA result
+        // nor tweeted via Nintendo Switch.
+        res.status(400).json({ message: "Not related to Ring Fit Adventure." });
       }
       break;
     }
